7646af2a2c680d3c4184feeac0ef2fb8ddfb2800
filomenoPonce580 <110742301+filomenoPonce580@users.noreply.github.com>
2023-02-07 10:17:57 -0800

    created file with working function

---
+++ main.js
@@ -0,0 +1,14 @@
+//first attempt
+function sumOfMinimums(arr) {
+  //create empty array for minimum values
+  let minimums = [];
+  
+  //find minimum value of each array inside large array
+  for(let i = 0; i<arr.length; i++){
+    //use sort method to sort small to large, push first value
+    minimums.push(arr[i].sort((a,b)=>a-b)[0])
+  }
+  
+  //return reduced value to get sum
+  return minimums.reduce((a, c) => a + c, 0)
+}
